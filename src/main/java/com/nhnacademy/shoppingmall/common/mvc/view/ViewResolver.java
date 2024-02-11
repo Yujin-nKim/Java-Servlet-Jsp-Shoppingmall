3442cae24b2f724f94dfbf10e7ba212f0836fa46
yujinKim <yujinkimvv@gmail.com>
2024-02-11 20:00:02 +0900

    9.ViewResolver 구현

--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/view/ViewResolver.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/view/ViewResolver.java
@@ -21,18 +21,26 @@ public class ViewResolver {
 
     public  String getPath(String viewName){
         //todo#6-1  postfix+viewNAme+postfix 반환 합니다.
-        return "";
+        if(viewName.startsWith("/")) {
+            // '/main/index'와 같이 요청이 오는 경우 / 제거
+            viewName = viewName.substring(1);
+        }
+        return prefix + viewName + postfix;
     }
 
     public boolean isRedirect(String viewName){
         //todo#6-2 REDIRECT_PREFIX가 포함되어 있는지 체크 합니다.
+        viewName = viewName.toLowerCase();
+        if(viewName.contains(REDIRECT_PREFIX)) {
+            return true;
+        }
         return false;
     }
 
     public String getRedirectUrl(String viewName){
         //todo#6-3 REDIRECT_PREFIX를 제외한 url을 반환 합니다.
-
-        return "";
+//        viewName = viewName.substring(0, 9);
+        return viewName.substring(REDIRECT_PREFIX.length());
     }
 
     public String getLayOut(String viewName){
@@ -41,7 +49,9 @@ public class ViewResolver {
            /admin/경로가 포함되었다면 DEFAULT_ADMIN_LAYOUT 반환 합니다.
            /admin/경로가 포함되어 있지않다면 DEFAULT_SHOP_LAYOUT 반환 합니다.
         */
-
+        if (viewName.contains("/admin/")) {
+            return DEFAULT_ADMIN_LAYOUT;
+        }
         return DEFAULT_SHOP_LAYOUT;
     }
 }
